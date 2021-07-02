3280805b51c418d9b4def5ee08f413dbdaefae2d
Ken <kencho.gigascience@gmail.com>
2021-07-02 16:40:38 +0800

    Add rclone check step

--- scripts/gsbackup.sh
+++ scripts/gsbackup.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/bash
 
-/usr/local/bin/rclone sync -i $1 google-drive:rclone_backup --verbose
+backup_date=`date "+%Y%m%d"`
+test_dir="/Volumes/kencho/rclone-implementation"
+source_dir="/Volumes/kencho/rclone-implementation/scripts/"
+destination_dir="google-drive:rclone_backup"
 
+#echo $backup_date
+#echo $test_dir/logs/$backup_date.log
+
+# To sync destination dir with source dir
+/usr/local/bin/rclone --checksum -vv sync $source_dir $destination_dir > $test_dir/logs/$backup_date.log 2>&1 || \
+  echo "rclone".$backup_date."failed!" >> $test_dir/logs/$backup_date.log
+
+# To check difference between source dir and destination dir
+/usr/local/bin/rclone check scripts/ google-drive:rclone_backup >> $test_dir/logs/$backup_date.log 2>&1
